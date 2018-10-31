ba816ca593a91e891d472e21aef3db6b3b482147
jay <jay.wang.scorpion@gmail.com>
2018-10-31 11:04:28 +0800

    init:smile:

---
+++ README.md
@@ -0,0 +1,8 @@
+# docker-flask-uwsgi-nginx
+
+```
+//BUILD IMAGE
+docker build -t IMAGE_NAME .
+
+docker-compose up|down
+```
---
+++ demo/app/app.py
@@ -0,0 +1,14 @@
+
+from flask import Flask
+import os
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    
+    return 'hello ' + os.getenv("NAME", "default?")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
